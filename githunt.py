3bd4e901ab6ce1c4bbdc82c9bfa36f26b42f9138
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-09-03 21:24:17 +0200

    Create githunt.py

---
+++ githunt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from assets.__main__ import *
+from resources.banner import banner
+from assets.colors import red, white, reset
+
+print(banner)					
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description=f"{white}Git{red}OSINT{white}: {red}Github OSINT tool developed by {white}rly0nheart{red}. https://github.com/{white}rlyonheart{reset}")
+	parser.add_argument("username", metavar="USERNAME", help="github username")
+	args = parser.parse_args()
+	username = args.username
+	Search().main(username)
+
+
