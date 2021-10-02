7965d7e978363140532c4e673917a4d15255daa1
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-10-03 00:51:34 +0200

    Update banner.py

--- resources/banner.py
+++ resources/banner.py
@@ -1,11 +1,11 @@
-from assets.colors import red,white,green,reset
+from lib.colors import red,white,green,reset
 
 banner = f"""{green}
- ▄▄ • ▪  ▄▄▄▄▄ ▄ .▄▄• ▄▌ ▐ ▄ ▄▄▄▄▄
-▐█ ▀ ▪██ •██  ██▪▐██▪██▌•█▌▐█•██  
-▄█ ▀█▄▐█· ▐█.▪██▀▐██▌▐█▌▐█▐▐▌ ▐█.▪
-▐█▄▪▐█▐█▌ ▐█▌·██▌▐▀▐█▄█▌██▐█▌ ▐█▌·
-·▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ · ▀▀▀ ▀▀ █▪ ▀▀▀ 
+╱╱╭┳╮╭╮╱╱╱╱╱╭╮
+╭━╋┫╰┫╰┳┳┳━┳┫╰╮
+┃╋┃┃╭┫┃┃┃┃┃┃┃╭┫
+┣╮┣┻━┻┻┻━┻┻━┻━╯
+╰━╯
 {red}Disclaimer{white}:
 This tool was developed for educational purposes and should not be used in environments
 without legal authorization.{reset}
